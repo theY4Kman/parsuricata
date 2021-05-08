ab0426ff1a82cf7e88e0148e8bf3fb94cf81dd2a
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2021-05-08 05:25:35 -0400

    fix: allow trailing newlines in source

--- test_parsuricata.py
+++ test_parsuricata.py
@@ -33,7 +33,7 @@ def test_negated_content():
     assert repr(option.settings) == "!'heymum'"
 
 
-def test_multiline_rules():
+def test_multiline_rule():
     rules = parse_rules('''
         alert ip any any -> any any ( \\
             content: "heymum"; \\
@@ -49,3 +49,36 @@ def test_multiline_rules():
     assert option.settings == 'heymum'
     assert not option.settings.is_negated
     assert repr(option.settings) == "'heymum'"
+
+
+def test_multiple_rules():
+    rules = parse_rules('''
+        alert ip any any -> any any (content: "a";)
+        alert ip any any -> any any ( content: "b"; \\
+        )
+        alert ip any any -> any any ( \\
+            content: "c"; \\
+        )
+        alert ip any any -> any any (content: !"d";)
+        alert ip any any -> any any ( content: !"e"; \\
+        )
+        alert ip any any -> any any ( \\
+            \\
+            content: !"f"; \\
+        )
+    ''')
+
+    expected = [
+        ('a', False),
+        ('b', False),
+        ('c', False),
+        ('d', True),
+        ('e', True),
+        ('f', True),
+    ]
+    actual = [
+        (option.settings, option.settings.is_negated)
+        for rule in rules
+        for option in rule.options
+    ]
+    assert expected == actual
